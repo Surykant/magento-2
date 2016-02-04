6ee4f4764a38f4611174e2d00382a8c3124e7e75
suryakant <suryakant.makwana@krishtechnolabs.com>
2016-02-04 12:01:55 +0530

    added missing files

---
+++ app/etc/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'install' => 
+  array (
+    'date' => 'Wed, 03 Feb 2016 10:35:38 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => '9cf0154f57c406e17352f9052fab01ea',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento2',
+        'username' => 'root',
+        'password' => 'root',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'full_page' => 1,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+);
